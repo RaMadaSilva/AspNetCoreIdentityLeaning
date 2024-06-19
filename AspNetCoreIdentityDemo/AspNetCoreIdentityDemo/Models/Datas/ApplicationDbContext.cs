c8c3aa43f968ea41d3bb8c9844354f55129a009c
RaMadaSilva <raul01mateia@gmail.com>
2024-06-19 14:30:15 +0100

    iniciando a passagem para um repositorio remoto

---
+++ AspNetCoreIdentityDemo/AspNetCoreIdentityDemo/Models/Datas/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentityDemo.Models.Datas;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext(options)
+{
+}
