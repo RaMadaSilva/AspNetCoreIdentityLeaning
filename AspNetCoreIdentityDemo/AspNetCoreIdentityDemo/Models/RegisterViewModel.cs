615263388818407e8a665ca91ef95cd6703b5eed
RaMadaSilva <raul01mateia@gmail.com>
2024-06-19 16:15:17 +0100

    COmo registar um novo user

---
+++ AspNetCoreIdentityDemo/AspNetCoreIdentityDemo/Models/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityDemo.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Password end Confirm password not match")]
+        public string ConfirmPassword { get; set; } = string.Empty; 
+    }
+}
