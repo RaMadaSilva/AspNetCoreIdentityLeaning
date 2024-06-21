d7e513e210ae39a05a77f59e3a5fa2d4f12cc75b
RaMadaSilva <raul01mateia@gmail.com>
2024-06-21 11:53:47 +0100

    Login e logout

--- AspNetCoreIdentityDemo/AspNetCoreIdentityDemo/Controllers/AccountController.cs
+++ AspNetCoreIdentityDemo/AspNetCoreIdentityDemo/Controllers/AccountController.cs
@@ -17,6 +17,37 @@ namespace AspNetCoreIdentityDemo.Controllers
         }
 
         [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager
+                        .PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Login Invalido");
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("index", "home"); 
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
---
+++ AspNetCoreIdentityDemo/AspNetCoreIdentityDemo/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityDemo.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+        [Display(Name ="Remeber Me")]
+        public bool RememberMe { get; set; }
+    }
+}
